771812fbc23313fbf3e63ba82b75a5ce60a522e9
Tetsuya Morimoto <tetsuya.morimoto@gmail.com>
2020-09-15 10:06:37 +0900

    refactor filter pages for optimization

--- mysite/note/wagtail_hooks.py
+++ mysite/note/wagtail_hooks.py
@@ -1,7 +1,5 @@
 from wagtail.core import hooks
 
-from .models import is_page_owner
-
 
 def is_root_page(page):
     return page.depth == 1
@@ -15,12 +13,8 @@ def my_hook(parent_page, pages, request):
     if request.user.is_superuser:
         return pages
 
-    filtered = []
-    for page in pages:
-        if is_page_owner(page, request.user):
-            filtered.append(page)
-    print(filtered)
-    return filtered
+    pages = pages.filter(owner=request.user.pk)
+    return pages
 
 
 @hooks.register('construct_main_menu')
